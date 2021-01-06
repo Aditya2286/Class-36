fc8fb9c82f8aaa036916a945b9e9782da1f84700
Your Name <you@example.com>
2021-01-06 14:04:38 +0530

    Added title

---
+++ js/player.js
@@ -0,0 +1,22 @@
+class Player{
+    constructor(){}
+    getCount(){
+        var gameStateRef=database.ref('playerCount');
+        gameStateRef.on("value",function(data){
+            playerCount=data.val();
+        })
+    }
+
+    updateCount(count){
+        database.ref('/').update({
+           playerCount:count
+        });
+    }
+
+   update(name){
+       var playerIndex="player"+playerCount;
+       database.ref(playerIndex).set({
+           name:name
+       })
+   }
+    }
